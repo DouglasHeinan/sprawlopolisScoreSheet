a6ad936237b83791ad968eb7e11bd20ca43479c5
gruvis41 <dougheinan@gmail.com>
2023-11-21 17:54:47 -0700

    removed obsolete files

--- sprawlopolis_score_sheet_express/spralOnly/index.js
+++ sprawlopolis_score_sheet_express/spralOnly/index.js
@@ -1,7 +1,6 @@
 const mongoose = require("mongoose");
 const ScoreCard = require("../models/scoringCards");
 const CardCombo = require("../models/cardCombos")
-// const GameResult = require("../models/gameResults")
 const cards = require("./seedSprawl")
 
 mongoose.connect("mongodb://127.0.0.1:27017/comboRecords", {
@@ -21,7 +20,6 @@ const updateCardDb = async () => {
     allCards =[];
     for (let i = 0; i < length; i++) {
         const card = cards["scoringCards"][i];
-        // const cardStats = await populateCardStats(card);
         const newCard = new ScoreCard({
             name: card["name"],
             description: card["description"],
@@ -33,11 +31,6 @@ const updateCardDb = async () => {
             minScore: card["min-score"],
             maxScore: card["max-score"],
             startingTotal: card["startingTotal"],
-            // mostPoints: cardStats["mostPoints"],
-            // fewestPoints: cardStats["fewestPoints"],
-            // gamesPlayed: cardStats["gamesPlayed"],
-            // wins: cardStats["wins"],
-            // losses: cardStats["losses"]
             mostPoints: 0,
             fewestPoints: 0,
             gamesPlayed: 0,
@@ -49,16 +42,6 @@ const updateCardDb = async () => {
     };
     return allCards;
 };
-
-// const populateCardStats = async (card) => {
-//     const cardStats = {};
-//     cardStats["fewestPoints"] = Math.floor(Math.random() * ((card["target"] - 3) - (card["target"] - 6)) + (card["target"] - 6));
-//     cardStats["mostPoints"] = Math.floor(Math.random() * ((card["target"] + 8) - (card["target"] + 2)) + (card["target"] + 2));
-//     cardStats["gamesPlayed"] = Math.floor(Math.random() * (20 - 3) + 3);
-//     cardStats["wins"] = cardStats["gamesPlayed"] - (Math.floor(Math.random() * cardStats["gamesPlayed"]));
-//     cardStats["losses"] = cardStats["gamesPlayed"] - cardStats["wins"];
-//     return cardStats;
-// }
 
 
 const createCombos = async () => {
@@ -80,7 +63,6 @@ const saveCombos = async () => {
     await CardCombo.deleteMany({});
     const allCombos = await createCombos();
     for (let combo of allCombos) {
-        // seeds = createStats();
         const comboTargetScore = combo[0].cardTargetScore + combo[1].cardTargetScore + combo[2].cardTargetScore
         const newCombo = new CardCombo({
             cards: combo,
@@ -95,61 +77,8 @@ const saveCombos = async () => {
     };
 };
 
-
-// const createStats = () => {
-//     const seeds = {};
-//     seeds["wins"] = Math.floor((Math.random() * 10) + 1);
-//     seeds["losses"] = Math.floor((Math.random() * 10) + 1);
-//     seeds["highScore"] = Math.floor(Math.random() * (35 - 25) + 25);
-//     seeds["lowScore"] = Math.floor(Math.random() * (15 - 10) + 10);
-//     seeds["avgScore"] = Math.random() * (seeds["highScore"] - seeds["lowScore"]) + seeds["lowScore"];
-//     return seeds;
-// };
-
-// const createResults = async () => {
-//     await GameResult.deleteMany({});
-//     for (let i = 0; i < 50; i++) {
-//         const record = await createRecord();
-//         const newResult = new GameResult({
-//             cardCombo: record["combo"],
-//             win: record["win"],
-//             score: record["score"],
-//             target: record["target"]
-//         });
-//         await newResult.save();
-//     };
-// };
-
-// const createRecord = async () => {
-//     const allCombos = await CardCombo.find({});
-//     const record = {};
-//     let win = false;
-//     const comboIndex = Math.floor(Math.random() * 816 );
-//     combo = allCombos[comboIndex]
-//     let score;
-//     if (comboIndex % 2 === 0) {
-//         win = true;
-//     };
-//     if (win) {
-//         score = Math.floor(Math.random() * (35 - 25) + 25);
-//     } else {
-//         score = Math.floor(Math.random() * (23 - 10) + 10);
-//     }
-//     record["combo"] = combo;
-//     record["win"] = win;
-//     record["target"] = 25;
-//     record["score"] = score;
-//     return record;
-// };
-
 makeThings = async () => {
     await saveCombos()
-    // .then(
-    //     await createResults()
-    // )
-    .then(() => {
-        mongoose.connection.close();
-    })
 };
 
 makeThings();
