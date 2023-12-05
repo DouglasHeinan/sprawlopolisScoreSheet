2e307b9cc2d949715238a80e4b1281e795dda1ff
gruvis41 <dougheinan@gmail.com>
2023-12-05 12:26:53 -0700

    moved combos.js routes logic to controller

--- sprawlopolis_score_sheet_express/controllers/combos.js
+++ sprawlopolis_score_sheet_express/controllers/combos.js
@@ -1,7 +1,111 @@
 const CardCombo = require("../models/cardCombos");
+const UserRecord = require("../models/userComboRecords")
+const GameResult = require("../models/gameResults")
 
 module.exports.index = async(req, res) => {
     const allCombos = await CardCombo.find({}).populate("cards");
     const someCombos = allCombos.slice(200);
     res.render("tempViews/tempViewCombos", {someCombos});
-}
+};
+
+module.exports.viewGames = async (req, res) => {
+    const {id} = req.params;
+    const combo = await CardCombo.findById(id).populate("cards");
+    if (!combo) {
+        req.flash("error", "Combination does not exist.")
+    }
+    const comboRecord = await UserRecord.findOne({ user: req.user._id, cardCombo: combo.id }).populate("gamesPlayed");
+    
+    // Should run when games are added/deleted
+    if (comboRecord && comboRecord.gamesPlayed.length > 0) {
+        numGames = comboRecord.gamesPlayed.length;
+        let totalScore = 0;
+        for (let i = 0; i < numGames; i++) {
+            const thisGame = await GameResult.findById(comboRecord.gamesPlayed[i])
+            totalScore += thisGame.score
+        }
+
+        comboRecord.avgScore = totalScore / numGames;
+        comboRecord.save()
+    }
+
+    res.render("tempViews/tempAddNewGame", {combo, comboRecord});
+};
+
+module.exports.addGame = async (req, res, next) => {
+    const {id} = req.params;
+    const combo = await CardCombo.findById(id);
+    const gameScore = req.body.result.score;
+    let gameWin = false;
+    if (gameScore >= combo.targetScore) {
+        gameWin = true;
+    }; 
+    const newResult = new GameResult({
+        cardCombo: combo.id,
+        user: req.user._id,
+        win : gameWin,
+        score: gameScore,
+        target: combo.targetScore
+    });
+    await newResult.save();
+    let currentRecord;
+    currentRecord = await UserRecord.findOne({ user: req.user._id, cardCombo: combo.id })
+    if (!currentRecord) {
+        currentRecord = new UserRecord({
+            user: req.user._id,
+            cardCombo: combo.id,
+            wins: 0,
+            losses: 0,
+            avgScore: 0,
+            highScore: 0
+        })
+    }
+    currentRecord.gamesPlayed.push(newResult);
+    if (gameWin) {
+        currentRecord.wins += 1;
+    } else {
+        currentRecord.losses += 1
+    };
+    numGames = currentRecord.gamesPlayed.length;
+    let totalScore = 0;
+    for (let i = 0; i < numGames; i++) {
+        const thisGame = await GameResult.findById(currentRecord.gamesPlayed[i])
+        totalScore += thisGame.score
+    }
+    currentRecord.avgScore = totalScore / numGames;
+    if (numGames > 1) {
+        const firstGame = await GameResult.findById(currentRecord.gamesPlayed[0])
+        let lowScore = firstGame.score;
+        for (let i = 1; i < numGames; i++) {
+            const compGame = await GameResult.findById(currentRecord.gamesPlayed[i])
+            if (compGame.score < lowScore) {
+                lowScore = compGame.score;
+            };
+        };
+        currentRecord.lowScore = lowScore;
+    }
+    if (gameScore > currentRecord.highScore) {
+        currentRecord.highScore = gameScore;
+    } else if (gameScore < currentRecord.lowScore) {
+        currentRecord.lowScore = gameScore;
+    };
+    await currentRecord.save();
+    req.flash("success", "Successfully added new game.")
+    res.redirect(`/combos/${id}/games/new`)
+};
+
+module.exports.deleteGame = async (req, res) => {
+    // needs validation so only user can delete
+    const { recordId, gameId } = req.params;
+    const comboRecord = await UserRecord.findByIdAndUpdate(recordId, { $pull: { gamesPlayed: gameId } })
+    const result = await GameResult.findByIdAndDelete(gameId)
+    if (result.win) {
+        comboRecord.wins -= 1;
+    } else {
+        comboRecord.losses -= 1;
+    };
+    comboRecord.save()
+    //Run high/low score calc***************
+    req.flash("success", "Successfully deleted game.")
+    res.redirect(`/combos/${comboRecord.cardCombo}/games/new`)
+};
--- sprawlopolis_score_sheet_express/routes/combos.js
+++ sprawlopolis_score_sheet_express/routes/combos.js
@@ -10,106 +10,11 @@ const UserRecord = require("../models/userComboRecords")
 
 router.get("/", catchAsync(combos.index));
 
-router.get("/:id/games/new", isLoggedIn, catchAsync(async (req, res) => {
-    const {id} = req.params;
-    const combo = await CardCombo.findById(id).populate("cards");
-    if (!combo) {
-        req.flash("error", "Combination does not exist.")
-    }
-    const comboRecord = await UserRecord.findOne({ user: req.user._id, cardCombo: combo.id }).populate("gamesPlayed");
-    
-    if (comboRecord && comboRecord.gamesPlayed.length > 0) {
-        numGames = comboRecord.gamesPlayed.length;
-        let totalScore = 0;
-        for (let i = 0; i < numGames; i++) {
-            const thisGame = await GameResult.findById(comboRecord.gamesPlayed[i])
-            totalScore += thisGame.score
-        }
+router.get("/:id/games/new", isLoggedIn, catchAsync(combos.viewGames));
 
-        comboRecord.avgScore = totalScore / numGames;
-        comboRecord.save()
-    }
-
-    res.render("tempViews/tempAddNewGame", {combo, comboRecord});
-}));
-
-router.post("/:id/games", isLoggedIn, catchAsync(async (req, res, next) => {
-    const {id} = req.params;
-    const combo = await CardCombo.findById(id);
-    const gameScore = req.body.result.score;
-    let gameWin = false;
-    if (gameScore >= combo.targetScore) {
-        gameWin = true;
-    }; 
-    const newResult = new GameResult({
-        cardCombo: combo.id,
-        user: req.user._id,
-        win : gameWin,
-        score: gameScore,
-        target: combo.targetScore
-    });
-    await newResult.save();
-    let currentRecord;
-    currentRecord = await UserRecord.findOne({ user: req.user._id, cardCombo: combo.id })
-    if (!currentRecord) {
-        currentRecord = new UserRecord({
-            user: req.user._id,
-            cardCombo: combo.id,
-            wins: 0,
-            losses: 0,
-            avgScore: 0,
-            highScore: 0
-        })
-    }
-    currentRecord.gamesPlayed.push(newResult);
-    if (gameWin) {
-        currentRecord.wins += 1;
-    } else {
-        currentRecord.losses += 1
-    };
-    numGames = currentRecord.gamesPlayed.length;
-    let totalScore = 0;
-    for (let i = 0; i < numGames; i++) {
-        const thisGame = await GameResult.findById(currentRecord.gamesPlayed[i])
-        totalScore += thisGame.score
-    }
-    currentRecord.avgScore = totalScore / numGames;
-    if (numGames > 1) {
-        const firstGame = await GameResult.findById(currentRecord.gamesPlayed[0])
-        let lowScore = firstGame.score;
-        for (let i = 1; i < numGames; i++) {
-            const compGame = await GameResult.findById(currentRecord.gamesPlayed[i])
-            if (compGame.score < lowScore) {
-                lowScore = compGame.score;
-            };
-        };
-        currentRecord.lowScore = lowScore;
-    }
-    if (gameScore > currentRecord.highScore) {
-        currentRecord.highScore = gameScore;
-    } else if (gameScore < currentRecord.lowScore) {
-        currentRecord.lowScore = gameScore;
-    };
-    await currentRecord.save();
-    req.flash("success", "Successfully added new game.")
-    res.redirect(`/combos/${id}/games/new`)
-}));
+router.post("/:id/games", isLoggedIn, catchAsync(combos.addGame));
 
 // Belong in games file?
-router.delete("/:recordId/games/:gameId", catchAsync(async (req, res) => {
-    // needs validation so only user can delete
-    const { recordId, gameId } = req.params;
-    const comboRecord = await UserRecord.findByIdAndUpdate(recordId, { $pull: { gamesPlayed: gameId } })
-    const result = await GameResult.findByIdAndDelete(gameId)
-    if (result.win) {
-        comboRecord.wins -= 1;
-    } else {
-        comboRecord.losses -= 1;
-    };
-    comboRecord.save()
-    //Run high/low score calc***************
-    req.flash("success", "Successfully deleted game.")
-    res.redirect(`/combos/${comboRecord.cardCombo}/games/new`)
-}));
+router.delete("/:recordId/games/:gameId", catchAsync(combos.deleteGame));
 
 module.exports = router;
